74e429436fbf437c71f508da82f79755dddadb2c
Maciej Wojno <git@mwojno.me>
2022-12-25 00:51:15 +0100

    add nixos-vm configuration

--- flake.nix
+++ flake.nix
@@ -1,30 +1,51 @@
 {
-  description = "Home Manager configuration for waciejm";
+  description = "Home and systems configurations for waciejm";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
   outputs = {
-    nixpkgs,
+    self,
+    nixpkgs-unstable,
+    nixpkgs-nixos,
     home-manager,
     ...
   }: {
-    packages.x86_64-linux.home-manager = home-manager.packages.x86_64-linux.home-manager;
-    packages.aarch64-darwin.home-manager = home-manager.packages.aarch64-darwin.home-manager;
-  
-    homeConfigurations.waciejm-macos = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-      modules = [ ./home/macos/home.nix ];
+    packages = {
+      aarch64-darwin.home-manager = home-manager.packages.aarch64-darwin.home-manager;
+      aarch64-linux.home-manager = home-manager.packages.aarch64-linux.home-manager;
+      x86_64-linux.home-manager = home-manager.packages.x86_64-linux.home-manager;
     };
 
-    homeConfigurations.waciejm-linux = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      modules = [ ./home/linux/home.nix ];
+    homeConfigurations = {
+      waciejm-macos = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-unstable.legacyPackages.aarch64-darwin;
+        modules = [ ./home/macos/home.nix ];
+      };
+      waciejm-linux = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
+        modules = [ ./home/linux/home.nix ];
+      };
+      waciejm-linux-arm = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-unstable.legacyPackages.aarch64-linux;
+        modules = [ ./home/linux/home.nix ];
+      };
+    };
+
+    nixosConfigurations = {
+      nixos-vm = nixpkgs-nixos.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./systems/nixos-vm/configuration.nix
+          {_module.args.flakeInputs.nixpkgs = nixpkgs-nixos;}
+        ];
+      };
     };
   };
 }
---
+++ systems/nixos-vm/configuration.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  pkgs,
+  flakeInputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  nix = {
+    registry.nixpkgs = {
+      from = {
+        type = "indirect";
+        id = "nixpkgs";
+      };
+      flake = flakeInputs.nixpkgs;
+    };
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      trusted-users = ["root" "@wheel"];
+    };
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-vm";
+
+  time.timeZone = "Europe/Warsaw";
+
+  console.keyMap = "pl";
+
+  users = {
+    mutableUsers = false;
+    users.waciejm = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "docker" ];
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keyFiles = [
+        (../../keys/ssh + "/waciejm@michair.pub")
+      ];
+    };
+  };
+
+  security.sudo = {
+    execWheelOnly = true;
+    wheelNeedsPassword = false;
+  };
+
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "prohibit-password";
+    passwordAuthentication = false;
+  };
+
+  networking.firewall.enable = false;
+
+  programs.zsh.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
