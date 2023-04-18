877664a230eafe40b40c5a445f41241b2ea47b2d
Maciej Wojno <git@mwojno.me>
2023-04-19 00:36:49 +0200

    badura: update configuration

--- systems/badura/configuration.nix
+++ systems/badura/configuration.nix
@@ -1,105 +1,97 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+  imports = [];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  system.stateVersion = "22.11";
 
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+    };
+    initrd = {
+      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+      luks.devices."luks-86002586-d1f5-46e3-adc9-2ed8b53fbe09".device = "/dev/disk/by-uuid/86002586-d1f5-46e3-adc9-2ed8b53fbe09";
+      secrets."/crypto_keyfile.bin" = null;
+    };
+    kernelModules = ["kvm-amd"];
   };
 
-  networking.hostName = "badura"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/664f52bc-4ab0-4a84-a504-042a9c124b4d";
+      fsType = "f2fs";
+    };
+    "/boot/efi" = {
+      device = "/dev/disk/by-uuid/2E7A-D3AD";
+      fsType = "vfat";
+    };
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
+    hostName = "badura";
+    useDHCP = true;
+  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  nixpkgs = {
+    hostPlatform = "x86_64-linux";
+    config.allowUnfree = true;
+  };
 
-  # Set your time zone.
+  hardware.cpu.amd.updateMicrocode = true;
+
   time.timeZone = "Europe/Warsaw";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
-    LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pl_PL.UTF-8";
+      LC_IDENTIFICATION = "pl_PL.UTF-8";
+      LC_MEASUREMENT = "pl_PL.UTF-8";
+      LC_MONETARY = "pl_PL.UTF-8";
+      LC_NAME = "pl_PL.UTF-8";
+      LC_NUMERIC = "pl_PL.UTF-8";
+      LC_PAPER = "pl_PL.UTF-8";
+      LC_TELEPHONE = "pl_PL.UTF-8";
+      LC_TIME = "pl_PL.UTF-8";
+    };
   };
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "pl";
     xkbVariant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "pl2";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.waciejm = {
-    isNormalUser = true;
-    description = "Maciej Wojno";
-    extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [];
+  users = {
+    mutableUsers = false;
+    users.waciejm = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "docker"];
+      openssh.authorizedKeys.keyFiles = [
+        (../../keys/ssh + "/waciejm@michair.pub")
+      ];
+      shell = pkgs.zsh;
+    };
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  programs = {
+    zsh.enable = true;
+    hyprland = {
+      enable = true;
+      xwayland = {
+        enable = true;
+        hidpi = true;
+      };
+    };
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  virtualisation.docker.enable = true;
 }
