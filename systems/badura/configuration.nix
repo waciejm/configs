520ed364adb576bd0623ab48c1803861b53cacea
Maciej Wojno <git@mwojno.me>
2023-04-18 23:11:44 +0200

    badura: change hostname to badura

--- systems/badura/configuration.nix
+++ systems/badura/configuration.nix
@@ -21,7 +21,7 @@
     "/crypto_keyfile.bin" = null;
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "badura"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
