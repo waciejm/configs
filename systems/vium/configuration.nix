ce78adea061d54dd757d14b63b0e6dc366151e5a
Maciej Wojno <git@mwojno.me>
2024-10-31 11:20:08 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'disko':
        'github:nix-community/disko/a6a3179ddf396dfc28a078e2f169354d0c137125?narHash=sha256-MuojlSnwAJAwfhgmW8ZtZrwm2Sko4fqubCvReqbUzYw%3D' (2024-10-18)
      → 'github:nix-community/disko/3979285062d6781525cded0f6c4ff92e71376b55?narHash=sha256-o5m5WzvY6cGIDupuOvjgNSS8AN6yP2iI9MtUC6q/uos%3D' (2024-10-29)
    • Updated input 'fenix':
        'github:nix-community/fenix/d6a9ff4d1e60c347a23bc96ccdb058d37a810541?narHash=sha256-Jkqaw9O7WXTf5SHrK7xr9HpVU/mEPVg0Sp6s3AENC90%3D' (2024-10-15)
      → 'github:nix-community/fenix/3a35e2c06a31ff4420639a454eeca4ccdfe69f13?narHash=sha256-pMW/yx6urzUimYdMHLGKRWxu2wmWMd7Dqz9oiGHBi8o%3D' (2024-10-31)
    • Updated input 'fenix/rust-analyzer-src':
        'github:rust-lang/rust-analyzer/0319586ef2a2636f6d6b891690b7ebebf4337c85?narHash=sha256-BOCZ5osPOMh2BPHnkK4sVdTGj7sn47rBn1nxjrzWe5U%3D' (2024-10-14)
      → 'github:rust-lang/rust-analyzer/8244f30eff828355f5ec92b2307c216d10caa25b?narHash=sha256-Kvve0NSLliNl1cv7qLSPdVWqLUEeAq0aZGsMeV632Cg%3D' (2024-10-30)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/122f70545b29ccb922e655b08acfe05bfb44ec68?narHash=sha256-KVyQq%2Bez/oB30/WbdNgVD8g/bda34z8NiU187QKQb74%3D' (2024-10-19)
      → 'github:nix-community/home-manager/e83414058edd339148dc142a8437edb9450574c8?narHash=sha256-bFCxJco7d8IgmjfNExNz9knP8wvwbXU4s/d53KOK6U0%3D' (2024-10-27)
    • Updated input 'nixos-cosmic':
        'github:lilyinstarlight/nixos-cosmic/a9621874555a327ad7702fc973ccdd6aa7738aa4?narHash=sha256-lPDL3PD96VUoksnVHs/57FQOIHkTCRf4X0Q7pkC7k40%3D' (2024-10-19)
      → 'github:lilyinstarlight/nixos-cosmic/bb2350119400c47be764c348e67f1b38e858435f?narHash=sha256-wwAKXZr5GU36NrVy/gERRWuQjIKvZYrTD5mRahd87vI%3D' (2024-10-31)
    • Updated input 'nixos-cosmic/nixpkgs-stable':
        'github:NixOS/nixpkgs/4eb33fe664af7b41a4c446f87d20c9a0a6321fa3?narHash=sha256-LDiPhQ3l%2BfBjRATNtnuDZsBS7hqoBtPkKBkhpoBHv3I%3D' (2024-10-17)
      → 'github:NixOS/nixpkgs/64b80bfb316b57cdb8919a9110ef63393d74382a?narHash=sha256-9z8oOgFZiaguj%2Bbbi3k4QhAD6JabWrnv7fscC/mt0KE%3D' (2024-10-28)
    • Updated input 'nixos-cosmic/rust-overlay':
        'github:oxalica/rust-overlay/9051466c82b9b3a6ba9e06be99621ad25423ec94?narHash=sha256-KDmYxpkFWa0Go0WnOpkgQOypVaQxbwgpEutET5ey1VQ%3D' (2024-10-18)
      → 'github:oxalica/rust-overlay/7509d76ce2b3d22b40bd25368b45c0a9f7f36c89?narHash=sha256-9pydem8OVxa0TwjUai1PJe0yHAJw556CWCEwyoAq8Ik%3D' (2024-10-30)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/38279034170b1e2929b2be33bdaedbf14a57bfeb?narHash=sha256-NU%2BtYe3QWzDNpB8RagpqR3hNQXn4BNuBd7ZGosMHLL8%3D' (2024-10-19)
      → 'github:NixOS/nixos-hardware/da14839ac5f38ee6adbdb4e6db09b5eef6d6ccdc?narHash=sha256-F8vJtG389i9fp3k2/UDYHMed3PLCJYfxCqwiVP7b9ig%3D' (2024-10-31)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/4c2fcb090b1f3e5b47eaa7bd33913b574a11e0a0?narHash=sha256-/uilDXvCIEs3C9l73JTACm4quuHUsIHcns1c%2BcHUJwA%3D' (2024-10-18)
      → 'github:NixOS/nixpkgs/807e9154dcb16384b1b765ebe9cd2bba2ac287fd?narHash=sha256-l253w0XMT8nWHGXuXqyiIC/bMvh1VRszGXgdpQlfhvU%3D' (2024-10-29)

--- systems/vium/configuration.nix
+++ systems/vium/configuration.nix
@@ -1,6 +1,5 @@
 {
   modulesPath,
-  pkgs,
   lib,
   ...
 }: {
@@ -20,7 +19,6 @@
       systemd.enable = true;
       availableKernelModules = ["ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod"];
     };
-    kernelPackages = pkgs.linuxPackages_6_1;
   };
 
   disko.devices.disk.vdi = {
