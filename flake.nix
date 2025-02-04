452e46ad7c9511d73f10223d2939b21ab217560f
Maciej Wojno <jj@mwojno.me>
2025-02-04 14:01:08 +0100

    Revert flake.lock updates to 2025-01-20

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     fenix = {
