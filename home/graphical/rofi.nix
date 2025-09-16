4b902ba81558c513f24cf7b474caf5f9e5054b24
Maciej Wojno <git@mwojno.me>
2025-09-16 11:09:48 +0200

    update

--- home/graphical/rofi.nix
+++ home/graphical/rofi.nix
@@ -1,13 +1,11 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   config = lib.mkIf config.waciejm.graphical {
     programs.rofi = {
       enable = true;
-      package = pkgs.rofi-wayland;
       theme = "purple";
       extraConfig = {
         show-icons = true;
