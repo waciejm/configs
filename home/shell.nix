621c42e2ff04a9ff4fe4a05058b6389e502a9017
Maciej Wojno <git@mwojno.me>
2025-10-19 18:47:12 +0200

    configure jj status in starship

--- home/shell.nix
+++ home/shell.nix
@@ -50,41 +50,118 @@
       enable = true;
     };
 
-    starship = {
+    starship = let
+      gitModules = [
+        "git_branch"
+        "git_commit"
+        "git_status"
+        "git_state"
+      ];
+    in {
       enable = true;
       enableZshIntegration = true;
       enableNushellIntegration = true;
       settings = {
-        format = ''
+        format = let          
+          vcs = lib.concatMapStrings (s: "\${custom.${s}}") (gitModules ++ [ "jj" ]);
+        in ''
           $time$username$hostname$directory$cmd_duration
-          $git_branch$git_status$git_state$rust$python$package
+          $shell${vcs}$rust$python$package
           $shlvl$status[ » ](bold green)
         '';
-        username = {
-          show_always = true;
-          format = "[$user]($style)";
-        };
-        hostname = {
-          ssh_only = false;
-          format = "[@$hostname ]($style)";
-        };
         time = {
           disabled = false;
           format = "[$time ]($style)";
         };
+        username = {
+          disabled = false;
+          show_always = true;
+          format = "[$user]($style)";
+        };
+        hostname = {
+          disabled = false;
+          ssh_only = false;
+          format = "[@$hostname ]($style)";
+        };
         directory = {
+          disabled = false;
           truncate_to_repo = false;
           format = "[$path]($style)[$read_only]($read_only_style) ";
         };
         cmd_duration = {
+          disabled = false;
           min_time = 1000;
+          show_milliseconds = true;
+        };
+        shell = {
+          disabled = false;
+          format = "[$indicator ]($style)";
+        };
+        custom = {
+          jj = {
+            description = "Show current jj status";
+            shell = [ "sh" ];
+            when = "jj root --ignore-working-copy";
+            format = "on $output ";
+            command = ''
+              jj log --no-graph --color always --revisions @ --template '
+                separate(" ",
+                  change_id.shortest(8),
+                  truncate_end(50, bookmarks, "…"),
+                  label("separator", "|"),
+                  concat(
+                    if(conflict, label("conflict", " ")),
+                    if(divergent, label("divergent", " ")),
+                    if(immutable, " "),
+                    if(hidden, "󰊠 "),
+                  ),
+                  if(empty, label("log commit empty", "(empty)")),
+                  label(
+                    separate(" ",
+                      "log commit",
+                      if(description.len() == 0 && empty, "empty"),
+                      "description",
+                      if(description.len() == 0, "placeholder"),
+                    ),
+                    coalesce(
+                      truncate_end(72, description.first_line(), "…"),
+                      "(no description set)",
+                    ),
+                  ),
+                )
+              '
+            '';
+          };
+        }
+        // lib.genAttrs gitModules (module: {
+          description = "Show ${module} only if we're not in a jj repo";
+          shell = [ "sh" ];
+          when = "! jj root --ignore-working-copy";
+          format = "($output )";
+          command = "starship module ${module}";
+        });
+        git_branch = {
+          disabled = false;
+          when = "! jj root --ignore-working-copy";
+        };
+        git_commit = {
+          disabled = false;
+          when = "! jj root --ignore-working-copy";
+        };
+        git_status = {
+          disabled = false;
+          when = "! jj root --ignore-working-copy";
+        };
+        git_state = {
+          disabled = false;
+          when = "! jj root --ignore-working-copy";
+        };
+        shlvl = {
+          disabled = false;
         };
         status = {
           disabled = false;
           format = "[$symbol $status]($style)";
-        };
-        shlvl = {
-          disabled = false;
         };
       };
     };
