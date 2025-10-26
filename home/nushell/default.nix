ffbe87272bdb1552b13f3dce6ef1a8ae3240e8b8
Maciej Wojno <git@mwojno.me>
2025-10-26 23:39:51 +0100

    nushell: fix nix conf files

--- home/nushell/default.nix
+++ home/nushell/default.nix
@@ -1,6 +1,12 @@
-{
+{ lib, config, ...}: {
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
+    environmentVariables = {
+      NIX_USER_CONF_FILES = lib.concatStringsSep ":" [
+        (config.xdg.configHome + "/nix/nix.conf")
+        (config.home.homeDirectory + "/Keys/nix-github-access.conf")
+      ];
+    };
   };
 }
