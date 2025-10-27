5a3e35ab6cf13f30bcda3b808e5bc256aa60893a
Maciej Wojno <git@mwojno.me>
2025-10-27 13:13:26 +0100

    make home.sessionVariables work with nushell

--- home/nushell/default.nix
+++ home/nushell/default.nix
@@ -1,12 +1,7 @@
-{ lib, config, ...}: {
+{ config, ...}: {
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
-    environmentVariables = {
-      NIX_USER_CONF_FILES = lib.concatStringsSep ":" [
-        (config.xdg.configHome + "/nix/nix.conf")
-        (config.home.homeDirectory + "/Keys/nix-github-access.conf")
-      ];
-    };
+    environmentVariables = config.home.sessionVariables;
   };
 }
