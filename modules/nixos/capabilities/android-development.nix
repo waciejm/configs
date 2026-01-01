b7123ec03ac9b7bf494dcf16fd0e4caaea434094
Maciej Wojno <git@mwojno.me>
2026-01-01 15:09:35 +0100

    nix flake update

--- modules/nixos/capabilities/android-development.nix
+++ modules/nixos/capabilities/android-development.nix
@@ -17,17 +17,8 @@
 
       environment.systemPackages = [
         pkgs.android-studio
+        pkgs.android-tools
       ];
 
-      programs.adb.enable = true;
-
-      users = lib.mkIf config.custom.users.enable {
-        groups.adbusers.members = [ config.custom.users.username ];
-      };
-
-      services.udev.extraRules = lib.mkIf config.custom.users.enable ''
-        SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="2d00", MODE="0666", GROUP="adbusers"
-        SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="2d01", MODE="0666", GROUP="adbusers"
-      '';
     };
 }
