8a371fb590939e7a54e46dcdb1e543b83fd839ce
Maciej Wojno <git@mwojno.me>
2026-03-09 21:41:31 +0100

    add android open accessory udev rules

--- modules/nixos/capabilities/android-development.nix
+++ modules/nixos/capabilities/android-development.nix
@@ -20,5 +20,9 @@
         pkgs.android-tools
       ];
 
+      services.udev.extraRules = ''
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="2d00", MODE="0660", GROUP="users", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="2d01", MODE="0660", GROUP="users", TAG+="uaccess"
+      '';
     };
 }
