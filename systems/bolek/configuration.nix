4233651ed9091711ebbb84d10a01742388f7782e
Maciej Wojno <git@mwojno.me>
2026-08-23 19:40:55 +0200

    bolek: enable gui

--- systems/bolek/configuration.nix
+++ systems/bolek/configuration.nix
@@ -13,7 +13,9 @@
     # keep-sorted start block=yes
     capabilities = {
       # keep-sorted start block=yes
+      bluetooth.enable = true;
       containerisation.enable = true;
+      gui.enable = true;
       # keep-sorted end
     };
     services = {
